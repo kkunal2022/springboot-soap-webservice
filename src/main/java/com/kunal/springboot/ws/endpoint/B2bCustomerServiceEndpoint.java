a0cb22cb136e78b7e77755c038b87439e73a21ca
Kumar Kunal <kkunal2022@gmail.com>
2022-10-28 23:04:43 +0300

    Displaying WSDL Endpoint

---
+++ src/main/java/com/kunal/springboot/ws/endpoint/B2bCustomerServiceEndpoint.java
@@ -0,0 +1,32 @@
+package com.kunal.springboot.ws.endpoint;
+
+import java.math.BigInteger;
+
+import com.kunal.springboot.ws.generated.CreateCustomerRequest;
+import com.kunal.springboot.ws.generated.CreateCustomerResponse;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+/**
+ * Kumar.Kunal
+ */
+
+@Endpoint
+public class B2bCustomerServiceEndpoint {
+	
+	@ResponsePayload
+	@PayloadRoot(localPart="createCustomerRequest", namespace="http://kunal.soapws.com/types/v1")
+	public CreateCustomerResponse createCustomer( @RequestPayload CreateCustomerRequest request) {
+		
+		CreateCustomerResponse response = new CreateCustomerResponse();
+		response.setCustomerID(BigInteger.ONE);
+		response.setDetails(request.getCustomerName() + " " + request.getCustomerCR());
+		response.setStatus("SUCCESS");
+		// Add the logic of CreateCustomerFault for catching dynamically the response status codes.
+		return response;
+	}
+
+}
